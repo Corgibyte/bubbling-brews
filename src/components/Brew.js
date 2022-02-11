8b5949bca42462a091a15b9fd69729eff3d6cb2a
Hannah Young <hannah@corgibyte.com>
2022-02-11 13:39:05 -0800

    Add styling for brew list

--- src/components/Brew.js
+++ src/components/Brew.js
@@ -1,25 +1,56 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const rowStyle = {
+  display: "grid",
+  gridTemplateRows: "1fr",
+  gridTemplateColumns: "4fr 1fr"
+};
+
+const brewStyle = {
+  border: "1px #81A1C1 solid",
+  borderRadius: "2rem",
+  backgroundColor: "#3B4252",
+  padding: "1rem",
+  margin: "0 1rem 1rem 0"
+};
+
+const vialStyle = {
+  textAlign: "center",
+  padding: "3rem 0"
+};
+
+const validVial = {
+  color: "#88C0D0",
+  fontSize: "2rem",
+  verticalAlign: "center"
+}
+
+const invalidVial = {
+  color: "#BF616A",
+  fontSize: "2rem",
+  verticalAlign: "center"
+}
+
 function Brew(props) {
   let vialRemoveState = null;
   if (props.vials > 0) {
     vialRemoveState = (
-      <div onClick={() => props.whenBrewVialRemoved(props.id)}>
-        <p>Remove Vial</p>
-      </div>
+      <div onClick={() => props.whenBrewVialRemoved(props.id)} style={{ ...brewStyle, ...vialStyle }}>
+        <p style={validVial}>Remove Vial</p>
+      </div >
     );
   } else {
     vialRemoveState = (
-      <div className="vial-removal invalid">
-        <p>Cauldron Empty</p>
+      <div className="vial-removal invalid" style={{ ...brewStyle, ...vialStyle }}>
+        <p style={invalidVial}>Cauldron Empty</p>
       </div>
     );
   }
 
   return (
-    <div className="brew-row">
-      <div onClick={() => props.whenBrewClicked(props.id)}>
+    <div className="brew-row" style={rowStyle}>
+      <div onClick={() => props.whenBrewClicked(props.id)} style={brewStyle}>
         <h3>{props.name}: {props.potionClass}</h3>
         <h4>${props.price}</h4>
         <h5>Flavor: {props.flavor}</h5>
