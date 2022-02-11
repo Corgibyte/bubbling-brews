fe44171daade2a65e6539850ddd3afd7188a00e6
Hannah Young <hannah@corgibyte.com>
2022-02-11 10:44:29 -0800

    Implement EditBrewForm component

---
+++ src/components/EditBrewForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditBrewForm(props) {
+  const { brew } = props;
+
+  function handleEditBrewFormSubmission(event) {
+    event.preventDeafult();
+    props.onEditBrew({
+      name: event.target.name.value,
+      potionClass: event.target.potionClass.value,
+      price: event.target.price.value,
+      flavor: event.target.flavor.value,
+      vials: brew.vials,
+      id: brew.id
+    });
+  }
+
+  return (
+    <ReusableForm
+      formSubmissionHandler={handleEditBrewFormSubmission}
+      buttonText="Update Brew" />
+  );
+}
+
+EditBrewForm.propTypes = {
+  brew: PropTypes.object,
+  onEditBrew: PropTypes.func
+};
+
+export default EditBrewForm;
