c5df7d7cdf976db3842e3c78de08d9b6db68d234
Hannah Young <hannah@corgibyte.com>
2022-02-11 14:33:40 -0800

    Style inputs

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,6 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const inputStyle = {
+  backgroundColor: "#434C5E",
+  color: "#E5E9F0",
+  fontSize: "1.25rem",
+  border: "solid #8FBCBB 1px",
+  borderRadius: "1rem",
+  padding: "0.5rem",
+  margin: "0.5rem"
+};
+
 function ReusableForm(props) {
   let nameValue = null;
   let potionClassValue = null;
@@ -16,10 +26,14 @@ function ReusableForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <input type="text" name="name" placeholder="Brew Name" defaultValue={nameValue} />
-        <input type="text" name="potionClass" placeholder="Potion Class" defaultValue={potionClassValue} />
-        <input type="number" name="price" placeholder="Vial Price" defaultValue={priceValue} />
-        <input type="text" name="flavor" placeholder="Brew Flavor" defaultValue={flavorValue} />
+        <div>
+          <input style={inputStyle} type="text" name="name" placeholder="Brew Name" defaultValue={nameValue} />
+          <input style={inputStyle} type="text" name="potionClass" placeholder="Potion Class" defaultValue={potionClassValue} />
+        </div>
+        <div>
+          <input style={inputStyle} type="number" name="price" placeholder="Vial Price" defaultValue={priceValue} />
+          <input style={inputStyle} type="text" name="flavor" placeholder="Brew Flavor" defaultValue={flavorValue} />
+        </div>
         <button type="submit">{props.buttonText}</button>
       </form>
     </React.Fragment>
