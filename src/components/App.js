0949cfdc57b0ad7d5f2384fd9daa58d92817ef33
Hannah Young <hannah@corgibyte.com>
2022-02-11 09:58:04 -0800

    Implement top hierarchy of components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import BrewControl from "./BrewControl";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BrewControl />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  return (
+    <h1>Bea's Bubbling Brews</h1>
+  );
+}
+
+export default Header;
