3086cc7ed8a655df11e5f5d57ad1dd7516ed50d0
Hannah Young <hannah@corgibyte.com>
2022-02-11 13:20:16 -0800

    Implement background color

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import BrewControl from "./BrewControl";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <style>{'body { background-color: #2E3440; color: #ECEFF4; }'}</style>
+      </Helmet>
       <Header />
       <BrewControl />
     </div>
